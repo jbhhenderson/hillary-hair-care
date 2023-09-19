2abafeb115727554efe5385c614e3eabc453aba5
Jackson Henderson <jbhhenderson@gmail.com>
2023-09-19 13:01:56 -0500

    added update and view appointment details functionality

--- HillaryHairCare/client/src/data/appointmentsData.js
+++ HillaryHairCare/client/src/data/appointmentsData.js
@@ -3,3 +3,15 @@ const _apiUrl = "/api/appointments";
 export const getAppointments = async () => {
     return await fetch(_apiUrl).then((res) => res.json());
 };
+
+export const getAppointment = async (id) => {
+    return await fetch(`${_apiUrl}/${id}`).then((res) => res.json());
+};
+
+export const updateAppointment = async (appointmentId, appointmentObj) => {
+    return await fetch(`${_apiUrl}/update/${appointmentId}`,{
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(appointmentObj)  
+})
+};
